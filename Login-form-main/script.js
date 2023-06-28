414e2e54db21fed9397b71d55edca20aa3561087
HryhorchukRoma <124773242+HryhorchukRoma@users.noreply.github.com>
2023-06-28 22:50:54 +0300

    Add files via upload

---
+++ Login-form-main/script.js
@@ -0,0 +1,61 @@
+"use strict";
+console.log(window);
+
+window.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".login-card-form"),
+    name = form.querySelector('[type="email"]'),
+    password = form.querySelector('[type = "password"]'),
+    remember = form.querySelector("#remcheck"),
+    rememberPassword = form.querySelector(".forgot");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const data = {
+      login: name.value,
+      password: password.value,
+    };
+    if (remember.checked) {
+      localStorage.setItem("user", JSON.stringify(data));
+    }
+  });
+  function helloUser(helper) {
+    const user = JSON.parse(localStorage.getItem("user"));
+    helper(user);
+  }
+
+  function helper(user) {
+    if (user) {
+      return alert(`Hello ${user.login}`);
+    }
+  }
+  helloUser(helper);
+
+  function showData() {
+    const data = JSON.parse(localStorage.getItem("user"));
+    name.value = data.login;
+    password.value = data.password;
+  }
+  showData();
+
+  function isValidEmail(email) {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  }
+
+  rememberPassword.addEventListener("click", (e) => {
+    document.querySelector(".forgotten").style = "display: flex;";
+  });
+  document.querySelector(".forgot-button").addEventListener("click", () => {
+    if (
+      document.querySelector(".forgot-password").value.length >= 8 &&
+      isValidEmail(document.querySelector(".forgot-mail").value === "true")
+    ) {
+      document.querySelector(".forgotten").style = "display: none;";
+      alert(
+        `we have sent a letter to ${
+          document.querySelector(".forgot-mail").value
+        }, please check to accept`
+      );
+    } else alert("Enter all of data");
+  });
+});
